edb6335567bb3c5a50c31dbea7a85dd3002322de
Gabriela Mattos <gabriela.mattos@agencia.baciaspcj.org.br>
2019-10-15 15:28:45 -0300

    first commit

---
+++ script.sql
@@ -0,0 +1,9 @@
+create table tbContratos(
+idContrato int not null primary key identity(1,1),
+tituloContrato varchar(250),
+objetoContrato varchar(250),
+numeroContrato int,
+valorPCJContrato decimal(30,5),
+valorPCJContrapartida decimal(30,5),
+nrProcessoContrato int
+
