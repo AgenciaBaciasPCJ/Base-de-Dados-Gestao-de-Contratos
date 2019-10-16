924baf9d55ea92092c33a1b43668ef1fb3ecfbed
Gabriela Nery <gneri94@gmail.com>
2019-10-16 11:34:02 -0300

    criacao-tbContratos

--- script.sql
+++ script.sql
@@ -8,6 +8,14 @@ valorPCJContrapartida decimal(30,5),
 nrProcessoContrato int,
 dataInicioContrato Date,
 dataFimContrato Date,
-lala int,
+prazoContrato int,
+prazoTipoContrato varchar(5),
+tipoLicitacaoPCJContrato varchar(30),
+idArea int foreign key references tbArea(idArea),
+idTipoDemand int foreign key references tbTipoDemand(idTipoDemand),
+idFontRecurs int foreign key references tbFontRecurs(idFontRecurs),
+idAcaoPAP int foreign key references tbAcaoPAP(idAcaoPAP)
+);
+
 
 
